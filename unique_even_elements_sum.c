56176771edca46defd3d0bef3232631237283b86
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-07-13 06:54:22 +0530

    #include<stdio.h> int main() {     int n,i,j,sum=0,c=0,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;                 if(c==1)                 {                     if(arr[i]%2==0)                     {                         sum+=arr[i];                     }                 }                 if(j > i)                arr[j] = 0;             }         }     }     printf("%d",sum);     return 0; }

--- unique_even_elements_sum.c
+++ unique_even_elements_sum.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int a[100],i,c=0,j,sum=0,n;
+    int n,i,j,sum=0,c=0,arr[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
         c=0;
         for(j=0;j<n;j++)
         {
-            if(a[i]==a[j])
+            if(arr[i]==arr[j])
             {
                 c++;
                 if(c==1)
-                { 
-                    if(a[i]%2==0)
+                {
+                    if(arr[i]%2==0)
                     {
-                        sum+=a[i];
+                        sum+=arr[i];
                     }
                 }
-                if(j>i)
-                a[j]=0;
+                if(j > i)
+               arr[j] = 0;
             }
         }
-        
     }
     printf("%d",sum);
     return 0;
